9281763904c77eab3e5265008afe041b6cd6b378
rovin0805 <qhdrn67@naver.com>
2020-09-21 14:17:58 +0900

    Containers of Container Presenter Pattern

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -8,6 +8,7 @@ import {
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 import Header from "./Header";
 
 export default () => (
@@ -16,8 +17,10 @@ export default () => (
       <Header />
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/tv" component={TV} />
+        <Route path="/tv" exact component={TV} />
         <Route path="/search" component={Search} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/tv/:id" component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
