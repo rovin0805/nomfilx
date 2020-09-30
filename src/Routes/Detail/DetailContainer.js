b637aa75ac716be30497048c937c8987b9bf4085
rovin0805 <qhdrn67@naver.com>
2020-09-30 15:15:03 +0900

    Presenters of Container Presenter Pattern

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -44,7 +44,6 @@ export default class extends React.Component {
 
   render() {
     const { result, error, loading } = this.state;
-    console.log(result);
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
